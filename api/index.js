0001b52c316cad39ea65cbe07e480cb776823168
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-02-26 21:40:32 +0530

    successfully connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 const server = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("mongodb is connected!!")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
 const PORT = 8080;
 server.listen(PORT,()=>{
     console.log(`server running on ${PORT}`)
