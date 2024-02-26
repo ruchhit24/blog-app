09634666be2a94d63eca21ee0408bc53bf1f4f37
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-02-26 19:10:20 +0530

    done with routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
  import React from 'react'
- 
+ import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import About from '../Pages/About'
+import SignUp from '../Pages/SignUp'
+import Dashboard from '../Pages/Dashboard'
+import Header from '../components/Header'
+import SignIn from '../Pages/SignIn'
  const App = () => {
    return (
-     <div className='text-center'>App</div>
+     <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+     </Routes>
+
+     </BrowserRouter>
    )
  }
  
