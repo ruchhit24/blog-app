1bac0adab45ef2d29a9774700017d94a1992e741
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-02-28 18:59:33 +0530

    added redux-persist functionality to the store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx' 
-import {Provider} from 'react-redux'
-import {store} from '../src/redux/store.js'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store.js";
+import { persistor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <Provider store={store}>
-      <App />
-   </Provider>
-    
-  </React.StrictMode>,
-)
+    <PersistGate persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
+);
