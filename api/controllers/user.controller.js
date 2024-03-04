d0b9c0f9c5509afec6ac3c5bb81d763ea81b254b
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-03-04 11:34:20 +0530

    done with Api route for delete user

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -56,3 +56,16 @@ export const updateUser = async(req,res,next)=>{
       }
 
 }
+
+export const deleteUser = async(req,res,next)=>{
+  if(req.user.id !== req.params.userId)
+  {
+    return next(errorHandler(403,"You are not allowed to delet this account!!"))
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+    res.status(200).json("User Deleted Successfully!!")
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router();
-import {test, updateUser} from '../controllers/user.controller.js'
+import {deleteUser, test, updateUser} from '../controllers/user.controller.js'
 import { verifyUser } from '../utils/verifyUser.js';
 
 router.get('/test',test);
 router.put('/update/:userId',verifyUser,updateUser)
+router.delete('/delete/:userId',verifyUser,deleteUser)
 
 
 export default router
